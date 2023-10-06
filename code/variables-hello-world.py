b728fc8152ab76ebd2378cc1082a291748fbf62c
Oswaldo TC <40254158+dev-oswld@users.noreply.github.com>
2023-10-06 02:48:42 +0000

    [Code] Examples

---
+++ code/variables-hello-world.py
@@ -0,0 +1,13 @@
+# Example 1
+print("Hello World!")
+
+# Example 2
+name = "Alice"
+print(f"Hello {name}!")
+
+# Example 3
+age = 25
+print(f"Hello {name}! You are {age} years old.")
+
+# Example 5
+print("Welcome to the future!")
